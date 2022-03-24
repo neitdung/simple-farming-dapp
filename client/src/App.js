d804e6b7367f8b732b9eef6421cf44b18a7cfcc2
dungnt <dungnt183721@gmail.com>
2022-03-24 23:08:28 +0700

    fix readme.md

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,65 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import './App.css'
-import {Button} from 'antd'
+import { Tabs, Button, List, Card } from "antd";
+import { login, logout } from "./utils";
+
+const { TabPane } = Tabs;
 
 function App() {
+  const [isSignIn, setIsSignIn] = useState(false)
+
+  useEffect(() => {
+    if (window.walletConnection.isSignedIn()) {
+      setIsSignIn(true)
+    }
+  }, [])
+  const signInButton = <Button onClick={() => login()}>Sign In</Button>
+  const signOutButton = <Button onClick={() => logout()}>Sign Out</Button>;
+  return (
+    <Tabs tabBarExtraContent={isSignIn ? signOutButton : signInButton} style={{padding: '10px 20px', margin: '20px', border: 'dashed 1px'}}>
+      <TabPane tab="Farm List" key="1">
+        <FarmList/>
+      </TabPane>
+      <TabPane tab="Create Farm" key="2">
+        Content of tab 2
+      </TabPane>
+    </Tabs>
+  );
+}
+
+const FarmList = () => {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    if (window.farmingContract) {
+      window.farmingContract
+        .list_farms({ from_index: 0, limit: 10 })
+        .then((farms) => {
+          if (farms && farms.length) {
+            setList(farms);
+          }
+        });
+    }
+  }, []);
 
   return (
-    <div>Hello</div>
-  )
+    <List
+      grid={{ gutter: 16, column: 4 }}
+      dataSource={list}
+      renderItem={(item) => (
+        <List.Item>
+          <Card title={item.farm_id}>
+            <p>Seed Reward: {item.seed_id}</p>
+            <p>Remain: {item.total_reward}</p>
+            <p>Claimed: {item.claimed_reward}</p>
+            <p>Start at: {item.start_at}</p>
+            <p>Reward per session: {item.reward_per_session}</p>
+            <p>Status: {item.farm_status}</p>
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
 }
 
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import 'antd/dist/antd.css'
 import { initContract } from './utils'
 
 window.nearInitPromise = initContract()
